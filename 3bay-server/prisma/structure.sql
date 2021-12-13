bd501c388bdba1e4238f99150f8149748b3e99ea
qhuydo <46428103+qhuydo@users.noreply.github.com>
2021-12-13 23:29:02 +0700

    [DB] Update `users`, `admin` table (again)
    [*] structure.sql:
      * Add index for `email` column, add `address` at `admins` table.
        Rename `id` into `uuid` .
        Remove default value of `uuid`, `refreshToken`.
      * Add `address` column at `users` table.
        Remove default value of `uuid`, `refreshToken`.
    [*] schema.prisma: add default value for the `uuid`, `refreshToken`
        at `admins` & `users` model.
        About uuid attribute functions:
        https://www.prisma.io/docs/reference/api-reference/prisma-schema-reference#uuid

--- 3bay-server/prisma/structure.sql
+++ 3bay-server/prisma/structure.sql
@@ -24,12 +24,14 @@ DROP TABLE IF EXISTS `admins`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `admins`
 (
-    `id`           varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `uuid`         varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
     `name`         varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
     `email`        varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-    `dob`          datetime                                         DEFAULT NULL,
-    `refreshToken` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT uuid(),
-    PRIMARY KEY (`id`)
+    `dob`          datetime                                DEFAULT NULL,
+    `refreshToken` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `address`      varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    PRIMARY KEY (`uuid`),
+    UNIQUE KEY `admins_email_uindex` (`email`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_unicode_ci;
@@ -172,6 +174,7 @@ CREATE TABLE `products`
     PRIMARY KEY (`id`),
     KEY `products_fk0` (`categoryId`),
     KEY `products_fk1` (`sellerId`),
+    FULLTEXT KEY `name` (`name`),
     CONSTRAINT `products_fk0` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`) ON UPDATE CASCADE,
     CONSTRAINT `products_fk1` FOREIGN KEY (`sellerId`) REFERENCES `users` (`uuid`) ON UPDATE CASCADE
 ) ENGINE = InnoDB
@@ -226,24 +229,70 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users`
 (
-    `uuid`         varchar(255) COLLATE utf8mb4_unicode_ci           NOT NULL DEFAULT uuid(),
-    `name`         varchar(255) COLLATE utf8mb4_unicode_ci           NOT NULL,
-    `email`        varchar(255) COLLATE utf8mb4_unicode_ci           NOT NULL,
-    `isDisabled`   tinyint(1)                                        NOT NULL DEFAULT 0,
-    `type`         enum ('BIDDER', 'SELLER') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'BIDDER',
-    `pwd`          varchar(255) COLLATE utf8mb4_unicode_ci           NOT NULL,
-    `dob`          datetime                                                   DEFAULT NULL,
-    `verified`     tinyint(1)                                        NOT NULL DEFAULT 0,
-    `profile`      varchar(255) COLLATE utf8mb4_unicode_ci                    DEFAULT NULL,
-    `refreshToken` varchar(255) COLLATE utf8mb4_unicode_ci           NOT NULL DEFAULT uuid(),
+    `uuid`         varchar(255) COLLATE utf8mb4_unicode_ci             NOT NULL,
+    `name`         varchar(255) COLLATE utf8mb4_unicode_ci             NOT NULL,
+    `email`        varchar(255) COLLATE utf8mb4_unicode_ci             NOT NULL,
+    `isDisabled`   tinyint(1)                                          NOT NULL DEFAULT 0,
+    `type`         enum ('BIDDER','SELLER') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'BIDDER',
+    `pwd`          varchar(255) COLLATE utf8mb4_unicode_ci             NOT NULL,
+    `dob`          datetime                                                     DEFAULT NULL,
+    `verified`     tinyint(1)                                          NOT NULL DEFAULT 0,
+    `profile`      varchar(255) COLLATE utf8mb4_unicode_ci                      DEFAULT NULL,
+    `refreshToken` varchar(255) COLLATE utf8mb4_unicode_ci             NOT NULL,
+    `address`      varchar(255) COLLATE utf8mb4_unicode_ci                      DEFAULT NULL,
     PRIMARY KEY (`uuid`),
     UNIQUE KEY `email` (`email`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_unicode_ci;
-
-ALTER table products ADD FULLTEXT (name);
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping events for database '3bay'
+--
+
+--
+-- Dumping routines for database '3bay'
+--
+/*!50003 DROP FUNCTION IF EXISTS `uuid_v4` */;
+/*!50003 SET @saved_cs_client = @@character_set_client */;
+/*!50003 SET @saved_cs_results = @@character_set_results */;
+/*!50003 SET @saved_col_connection = @@collation_connection */;
+/*!50003 SET character_set_client = utf8mb4 */;
+/*!50003 SET character_set_results = utf8mb4 */;
+/*!50003 SET collation_connection = utf8mb4_general_ci */;
+/*!50003 SET @saved_sql_mode = @@sql_mode */;
+/*!50003 SET sql_mode = 'STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */;
+DELIMITER ;;
+CREATE
+    DEFINER = `root`@`localhost` FUNCTION `uuid_v4`() RETURNS char(36) CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci
+    NO SQL
+BEGIN
+    -- Generate 8 2-byte strings that we will combine into a UUIDv4
+    SET @h1 = LPAD(HEX(FLOOR(RAND() * 0xffff)), 4, '0');
+    SET @h2 = LPAD(HEX(FLOOR(RAND() * 0xffff)), 4, '0');
+    SET @h3 = LPAD(HEX(FLOOR(RAND() * 0xffff)), 4, '0');
+    SET @h6 = LPAD(HEX(FLOOR(RAND() * 0xffff)), 4, '0');
+    SET @h7 = LPAD(HEX(FLOOR(RAND() * 0xffff)), 4, '0');
+    SET @h8 = LPAD(HEX(FLOOR(RAND() * 0xffff)), 4, '0');
+
+    -- 4th section will start with a 4 indicating the version
+    SET @h4 = CONCAT('4', LPAD(HEX(FLOOR(RAND() * 0x0fff)), 3, '0'));
+
+    -- 5th section first half-byte can only be 8, 9 A or B
+    SET @h5 = CONCAT(HEX(FLOOR(RAND() * 4 + 8)),
+                     LPAD(HEX(FLOOR(RAND() * 0x0fff)), 3, '0'));
+
+    -- Build the complete UUID
+    RETURN LOWER(CONCAT(
+            @h1, @h2, '-', @h3, '-', @h4, '-', @h5, '-', @h6, @h7, @h8
+        ));
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode = @saved_sql_mode */;
+/*!50003 SET character_set_client = @saved_cs_client */;
+/*!50003 SET character_set_results = @saved_cs_results */;
+/*!50003 SET collation_connection = @saved_col_connection */;
 /*!40103 SET TIME_ZONE = @OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE = @OLD_SQL_MODE */;
@@ -254,4 +303,4 @@ ALTER table products ADD FULLTEXT (name);
 /*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES = @OLD_SQL_NOTES */;
 
--- Dump completed on 2021-12-13 16:46:40
+-- Dump completed on 2021-12-13 23:21:57
