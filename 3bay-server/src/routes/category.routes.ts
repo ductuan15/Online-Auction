22b9e3c281f8ab37276bfb89fd30cc571b3bc03e
qhuydo <46428103+qhuydo@users.noreply.github.com>
2021-12-14 03:03:45 +0700

    [BE] Add authentication in `category` api

--- 3bay-server/src/routes/category.routes.ts
+++ 3bay-server/src/routes/category.routes.ts
@@ -1,17 +1,31 @@
 import * as express from 'express'
 import categoryController from '../controllers/category.controller.js'
 import { uploadCategoryThumbnail } from '../middlewares/upload-category.mdw.js'
+import passport from '../auth/passport.js'
 
 const router = express.Router()
 
-router.route('/api/category')
+router
+  .route('/api/category')
   .get(categoryController.findAll)
-  .post(uploadCategoryThumbnail.single('thumbnail'), categoryController.add)
+  .post(
+    passport.authenticate('jwt', { session: false }),
+    uploadCategoryThumbnail.single('thumbnail'),
+    categoryController.add,
+  )
 
-router.route('/api/category/:categoryId')
+router
+  .route('/api/category/:categoryId')
   .get(categoryController.read)
-  .patch(uploadCategoryThumbnail.single('thumbnail'), categoryController.update)
-  .delete(categoryController.deleteCategory)
+  .patch(
+    passport.authenticate('jwt', { session: false }),
+    uploadCategoryThumbnail.single('thumbnail'),
+    categoryController.update,
+  )
+  .delete(
+    passport.authenticate('jwt', { session: false }),
+    categoryController.deleteCategory,
+  )
 
 router.param('categoryId', categoryController.categoryById)
 
