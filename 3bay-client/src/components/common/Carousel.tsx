d5d65858c29e9ae6d2ba6f80434e7f51d2be9eca
kiethoang214 <kiethoang611@gmail.com>
2021-12-15 18:41:43 +0700

    [FE] Add more properties for CardProduct

--- 3bay-client/src/components/common/Carousel.tsx
+++ 3bay-client/src/components/common/Carousel.tsx
@@ -11,61 +11,105 @@ export default function CarouselCard()
         {
             id: 1,
             title: "product 1",
-            price: 45000,
+            present_price: 45000,
+            rate: 4.5,
+            buy_now_price: 100000,
+            number_bidder: 123456,
+            date: "2021-12-11",
+            time: "19:00:00",
             image: "https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.15752-9/261136866_3212007769028490_6108586411649421599_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=ae9488&_nc_ohc=NK6KdSpdGKMAX_Zd5WE&_nc_ht=scontent.fsgn2-4.fna&oh=03_AVLzVcc4x1wJ0YUxlg4ruC2Ao3bLyijqKJEN8_gqFabVoA&oe=61DE6B0D"
         },
         {
             id: 2,
             title: "product 2",
-            price: 45000,
+            present_price: 45000,
+            rate: 4.5,
+            number_bidder: 123456,
+            date: "2021-12-11",
+            time: "19:00:00",
             image: "https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.15752-9/261136866_3212007769028490_6108586411649421599_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=ae9488&_nc_ohc=NK6KdSpdGKMAX_Zd5WE&_nc_ht=scontent.fsgn2-4.fna&oh=03_AVLzVcc4x1wJ0YUxlg4ruC2Ao3bLyijqKJEN8_gqFabVoA&oe=61DE6B0D"
         },
         {
             id: 3,
             title: "product 3",
-            price: 45000,
+            present_price: 45000,
+            rate: 4.5,
+            buy_now_price: 100000,
+            number_bidder: 123456,
+            date: "2021-12-11",
+            time: "19:00:00",
             image: "https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.15752-9/261136866_3212007769028490_6108586411649421599_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=ae9488&_nc_ohc=NK6KdSpdGKMAX_Zd5WE&_nc_ht=scontent.fsgn2-4.fna&oh=03_AVLzVcc4x1wJ0YUxlg4ruC2Ao3bLyijqKJEN8_gqFabVoA&oe=61DE6B0D"
         },
         {
             id: 4,
             title: "product 4",
-            price: 45000,
+            present_price: 45000,
+            rate: 4.5,
+            number_bidder: 123456,
+            date: "2021-12-11",
+            time: "19:00:00",
             image: "https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.15752-9/261136866_3212007769028490_6108586411649421599_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=ae9488&_nc_ohc=NK6KdSpdGKMAX_Zd5WE&_nc_ht=scontent.fsgn2-4.fna&oh=03_AVLzVcc4x1wJ0YUxlg4ruC2Ao3bLyijqKJEN8_gqFabVoA&oe=61DE6B0D"
         },
         {
             id: 5,
             title: "product 5",
-            price: 45000,
+            present_price: 45000,
+            rate: 3.5,
+            number_bidder: 123456,
+            date: "2021-12-11",
+            time: "19:00:00",
             image: "https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.15752-9/261136866_3212007769028490_6108586411649421599_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=ae9488&_nc_ohc=NK6KdSpdGKMAX_Zd5WE&_nc_ht=scontent.fsgn2-4.fna&oh=03_AVLzVcc4x1wJ0YUxlg4ruC2Ao3bLyijqKJEN8_gqFabVoA&oe=61DE6B0D"
         },
         {
             id: 6,
             title: "product 6",
-            price: 45000,
+            present_price: 45000,
+            rate: 4.5,
+            buy_now_price: 100000,
+            number_bidder: 123456,
+            date: "2021-12-11",
+            time: "19:00:00",
             image: "https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.15752-9/261136866_3212007769028490_6108586411649421599_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=ae9488&_nc_ohc=NK6KdSpdGKMAX_Zd5WE&_nc_ht=scontent.fsgn2-4.fna&oh=03_AVLzVcc4x1wJ0YUxlg4ruC2Ao3bLyijqKJEN8_gqFabVoA&oe=61DE6B0D"
         },
         {
             id: 7,
             title: "product 7",
-            price: 45000,
+            present_price: 45000,
+            rate: 3.5,
+            number_bidder: 123456,
+            date: "2021-12-11",
+            time: "19:00:00",
             image: "https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.15752-9/261136866_3212007769028490_6108586411649421599_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=ae9488&_nc_ohc=NK6KdSpdGKMAX_Zd5WE&_nc_ht=scontent.fsgn2-4.fna&oh=03_AVLzVcc4x1wJ0YUxlg4ruC2Ao3bLyijqKJEN8_gqFabVoA&oe=61DE6B0D"
         },
         {
             id: 8,
             title: "product 8",
-            price: 45000,
+            present_price: 45000,
+            rate: 4.5,
+            number_bidder: 123456,
+            date: "2021-12-11",
+            time: "19:00:00",
             image: "https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.15752-9/261136866_3212007769028490_6108586411649421599_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=ae9488&_nc_ohc=NK6KdSpdGKMAX_Zd5WE&_nc_ht=scontent.fsgn2-4.fna&oh=03_AVLzVcc4x1wJ0YUxlg4ruC2Ao3bLyijqKJEN8_gqFabVoA&oe=61DE6B0D"
         },
         {
             id: 9,
             title: "product 9",
-            price: 45000,
+            present_price: 45000,
+            rate: 3.5,
+            buy_now_price: 100000,
+            number_bidder: 123456,
+            date: "2021-12-11",
+            time: "19:00:00",
             image: "https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.15752-9/261136866_3212007769028490_6108586411649421599_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=ae9488&_nc_ohc=NK6KdSpdGKMAX_Zd5WE&_nc_ht=scontent.fsgn2-4.fna&oh=03_AVLzVcc4x1wJ0YUxlg4ruC2Ao3bLyijqKJEN8_gqFabVoA&oe=61DE6B0D"
         },
         {
             id: 10,
             title: "product 10",
-            price: 45000,
+            present_price: 45000,
+            rate: 4.5,
+            number_bidder: 123456,
+            date: "2021-12-11",
+            time: "19:00:00",
             image: "https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.15752-9/261136866_3212007769028490_6108586411649421599_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=ae9488&_nc_ohc=NK6KdSpdGKMAX_Zd5WE&_nc_ht=scontent.fsgn2-4.fna&oh=03_AVLzVcc4x1wJ0YUxlg4ruC2Ao3bLyijqKJEN8_gqFabVoA&oe=61DE6B0D"
         },
     ]
@@ -97,7 +141,7 @@ export default function CarouselCard()
                 // ssr={true} // means to render carousel on server-side.
                 infinite
                 autoPlay
-                autoPlaySpeed={4000}
+                autoPlaySpeed={3000}
                 customTransition="transform 300ms ease-in-out"
                 transitionDuration={300}
                 containerClass="container-with-dots"
