7952c9b288cd974e0e3be5496e9a8c72980bf072
qhuydo <46428103+qhuydo@users.noreply.github.com>
2021-12-17 01:17:50 +0700

    [DB] Create `OTP` table

--- 3bay-server/prisma/structure.sql
+++ 3bay-server/prisma/structure.sql
@@ -7,19 +7,13 @@
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE = @@TIME_ZONE */;
 /*!40103 SET TIME_ZONE = '+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0 */;
 /*!40101 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES = @@SQL_NOTES, SQL_NOTES = 0 */;
-
---
--- Table structure for table `admins`
---
-
-DROP TABLE IF EXISTS `admins`;
 
 --
 -- Table structure for table `auctions`
@@ -99,6 +93,24 @@ CREATE TABLE `categories`
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `otp`
+--
+
+DROP TABLE IF EXISTS `otp`;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `otp`
+(
+    `id`         varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `otp`        varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `expiryTime` datetime                                NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `product_des_history`
 --
 
@@ -156,9 +168,9 @@ CREATE TABLE `products`
     `deletedAt`    datetime                                         DEFAULT NULL,
     `currentPrice` decimal(19, 4)                          NOT NULL,
     PRIMARY KEY (`id`),
-    FULLTEXT KEY `name` (`name`),
     KEY `products_fk0` (`categoryId`),
     KEY `products_fk1` (`sellerId`),
+    FULLTEXT KEY `name` (`name`),
     CONSTRAINT `products_fk0` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`) ON UPDATE CASCADE,
     CONSTRAINT `products_fk1` FOREIGN KEY (`sellerId`) REFERENCES `users` (`uuid`) ON UPDATE CASCADE
 ) ENGINE = InnoDB
@@ -231,52 +243,6 @@ CREATE TABLE `users`
   COLLATE = utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping events for database '3bay'
---
-
---
--- Dumping routines for database '3bay'
---
-/*!50003 DROP FUNCTION IF EXISTS `uuid_v4` */;
-/*!50003 SET @saved_cs_client = @@character_set_client */;
-/*!50003 SET @saved_cs_results = @@character_set_results */;
-/*!50003 SET @saved_col_connection = @@collation_connection */;
-/*!50003 SET character_set_client = utf8mb4 */;
-/*!50003 SET character_set_results = utf8mb4 */;
-/*!50003 SET collation_connection = utf8mb4_general_ci */;
-/*!50003 SET @saved_sql_mode = @@sql_mode */;
-/*!50003 SET sql_mode = 'STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */;
-DELIMITER ;;
-CREATE
-    DEFINER = `root`@`localhost` FUNCTION `uuid_v4`() RETURNS char(36) CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci
-    NO SQL
-BEGIN
-    -- Generate 8 2-byte strings that we will combine into a UUIDv4
-    SET @h1 = LPAD(HEX(FLOOR(RAND() * 0xffff)), 4, '0');
-    SET @h2 = LPAD(HEX(FLOOR(RAND() * 0xffff)), 4, '0');
-    SET @h3 = LPAD(HEX(FLOOR(RAND() * 0xffff)), 4, '0');
-    SET @h6 = LPAD(HEX(FLOOR(RAND() * 0xffff)), 4, '0');
-    SET @h7 = LPAD(HEX(FLOOR(RAND() * 0xffff)), 4, '0');
-    SET @h8 = LPAD(HEX(FLOOR(RAND() * 0xffff)), 4, '0');
-
-    -- 4th section will start with a 4 indicating the version
-    SET @h4 = CONCAT('4', LPAD(HEX(FLOOR(RAND() * 0x0fff)), 3, '0'));
-
-    -- 5th section first half-byte can only be 8, 9 A or B
-    SET @h5 = CONCAT(HEX(FLOOR(RAND() * 4 + 8)),
-                     LPAD(HEX(FLOOR(RAND() * 0x0fff)), 3, '0'));
-
-    -- Build the complete UUID
-    RETURN LOWER(CONCAT(
-            @h1, @h2, '-', @h3, '-', @h4, '-', @h5, '-', @h6, @h7, @h8
-        ));
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode = @saved_sql_mode */;
-/*!50003 SET character_set_client = @saved_cs_client */;
-/*!50003 SET character_set_results = @saved_cs_results */;
-/*!50003 SET collation_connection = @saved_col_connection */;
 /*!40103 SET TIME_ZONE = @OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE = @OLD_SQL_MODE */;
@@ -287,4 +253,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES = @OLD_SQL_NOTES */;
 
--- Dump completed on 2021-12-14  2:23:20
+-- Dump completed on 2021-12-17  1:14:24
