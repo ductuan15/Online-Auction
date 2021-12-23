25c042f2588737e28e5497e7c0f99462a86c074b
qhuydo <46428103+qhuydo@users.noreply.github.com>
2021-12-24 00:48:21 +0700

    [BE] Update `admin/users` GET request

--- 3bay-server/src/controllers/admin.controller.ts
+++ 3bay-server/src/controllers/admin.controller.ts
@@ -11,8 +11,8 @@ const userDefaultSelection = {
   dob: true,
   verified: true,
   address: true,
+  profile: true,
   // pwd: false,
-  // profile: false,
   // refreshToken: false,
 }
 
@@ -23,13 +23,17 @@ export async function getUsers(
 ) {
   try {
     const page = +(req.query?.page || '/') || 1
+    const limit = +(req.query?.limit || '/') || config.USER_PAGE_LIMIT
 
-    const users = await prisma.user.findMany({
-      select: userDefaultSelection,
-      skip: (page - 1) * config.USER_PAGE_LIMIT,
-      take: config.USER_PAGE_LIMIT,
-    })
-    return res.json(users)
+    const [total, users] = await prisma.$transaction([
+      prisma.user.count(),
+      prisma.user.findMany({
+        select: userDefaultSelection,
+        skip: (page - 1) * limit,
+        take: limit,
+      }),
+    ])
+    return res.json({ total, users })
   } catch (e) {
     return next(e)
   }
@@ -45,7 +49,7 @@ export async function updateUser(
     const user = await prisma.user.update({
       where: { uuid: uuid },
       data: { ...data },
-      select: userDefaultSelection
+      select: userDefaultSelection,
     })
     return res.json(user)
   } catch (e) {
