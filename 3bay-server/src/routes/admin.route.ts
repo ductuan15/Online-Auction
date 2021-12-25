3765a6e25cfd94f710a49b5728e1b0af3643216e
qhuydo <46428103+qhuydo@users.noreply.github.com>
2021-12-25 21:36:03 +0700

    [BE] Add user

--- 3bay-server/src/routes/admin.route.ts
+++ 3bay-server/src/routes/admin.route.ts
@@ -1,6 +1,6 @@
 import * as express from 'express'
 import passport from '../auth/passport.js'
-import { requireAdminRole } from '../middlewares/auth.mdw.js'
+import { hashPassword, requireAdminRole } from '../middlewares/auth.mdw.js'
 import {
   deleteUser,
   getUsers,
@@ -8,6 +8,8 @@ import {
 } from '../controllers/admin.controller.js'
 import validate from '../middlewares/ajv-validate.js'
 import updateUserSchema from '../schemas/update-user.js'
+import addUserSchema from '../schemas/add-user.js'
+import { signUp } from '../controllers/auth.controller.js'
 
 const router = express.Router()
 
@@ -17,6 +19,7 @@ router
   .route('/users')
   .get(getUsers)
   .patch(validate(updateUserSchema), updateUser)
+  .post(validate(addUserSchema), hashPassword, signUp)
 
 router.route('/users/:id').delete(deleteUser)
 
