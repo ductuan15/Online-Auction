c1064f367e77e58a34fe2597ac38723ac4bf1a6e
qhuydo <46428103+qhuydo@users.noreply.github.com>
2021-12-25 17:51:13 +0700

    [FE] Hotfix! Fix fontsource issue

--- 3bay-client/src/theme/index.tsx
+++ 3bay-client/src/theme/index.tsx
@@ -7,7 +7,6 @@ import {
 import { darkPalette, palette } from './palette'
 import { CssBaseline, PaletteMode } from '@mui/material'
 import { useDarkMode } from 'usehooks-ts'
-import '@fontsource/manrope'
 
 // import useEnhancedEffect from '@mui/material/utils/useEnhancedEffect'
 
@@ -26,20 +25,6 @@ export default function ThemeConfig(props: Props): JSX.Element {
       ...(mode === 'light' ? palette : darkPalette),
     },
     typography: { fontFamily: 'Manrope, sans-serif' },
-    components: {
-      MuiCssBaseline: {
-        styleOverrides: `
-        @font-face {
-          font-family: 'Manrope';
-          font-style: normal;
-          font-display: swap;
-          font-weight: 400;
-          src: url('https://fonts.googleapis.com/css2?family=Manrope:wght@450&display=swap');
-          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
-        }
-      `,
-      },
-    },
   })
 
   const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode])
--- 3bay-client/src/theme/palette.ts
+++ 3bay-client/src/theme/palette.ts
@@ -1,5 +1,4 @@
 import { alpha } from '@mui/material/styles'
-import '@fontsource/manrope'
 
 function createGradient(color1: string, color2: string) {
   return `linear-gradient(to bottom, ${color1}, ${color2})`
