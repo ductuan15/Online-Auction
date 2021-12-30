e7ce22e367a1a42f5fd7786caf4d3a1501dc869d
qhuydo <46428103+qhuydo@users.noreply.github.com>
2021-12-31 01:34:32 +0700

    [FE] Create `WYSISYGEditor` component
    Bad code, don't read this.
    I hate my life.

---
+++ 3bay-client/src/components/common/editor/WYSIWYGEditor.tsx
@@ -0,0 +1,96 @@
+import { ControllerRenderProps } from 'react-hook-form/dist/types/controller'
+import * as React from 'react'
+import { convertToRaw, EditorState } from 'draft-js'
+import draftToHtml from 'draftjs-to-html'
+import { Editor, EditorProps } from 'react-draft-wysiwyg'
+import { useTheme } from '@mui/material/styles'
+
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
+import './WYSIWYGEditor.css'
+
+type WYSIWYGEditorProps = {
+  // value?: string
+  onChange: ControllerRenderProps['onChange']
+} & EditorProps
+
+const TOOLBAR_OPTIONS: EditorProps['toolbar'] = {
+  options: [
+    'inline',
+    'blockType',
+    'fontSize',
+    'fontFamily',
+    'list',
+    'textAlign',
+    'colorPicker',
+    'emoji',
+    'remove',
+    'history',
+  ],
+  blockType: {
+    className: 'option-custom',
+  },
+  fontSize: {
+    options: [12, 14, 16, 18, 24, 30, 36, 48],
+  },
+  fontFamily: {
+    options: [
+      'Manrope',
+      'Roboto',
+      'Jetbrains Mono',
+      'Roboto Slab',
+      'Libre Baskerville',
+    ],
+  },
+  colorPicker: {
+    inDropdown: false,
+  },
+  link: {
+    inDropdown: false,
+  },
+  embedded: {
+    inDropdown: false,
+  },
+  image: {},
+}
+
+export default function WYSIWYGEditor({
+  // value,
+  onChange,
+  ...editorProps
+}: WYSIWYGEditorProps): JSX.Element {
+  // const [editorState, setEditorState] = useState(() => {
+  //   return EditorState.createEmpty()
+  // })
+
+  const theme = useTheme()
+  const toolbarClasses =
+    theme.palette.mode === 'light'
+      ? 'rdw-editor-toolbar-light'
+      : 'rdw-editor-toolbar-dark'
+
+  const onEditorStateChange = (editorState: EditorState) => {
+    // console.log('Hei')
+    // setEditorState(editorState)
+    return onChange(draftToHtml(convertToRaw(editorState.getCurrentContent())))
+  }
+
+  return (
+    <div
+      className='editor'
+
+    >
+      <Editor
+        // editorState={editorState}
+        wrapperClassName='wrapper-class'
+        editorClassName='editor-class'
+        toolbarClassName={toolbarClasses}
+        toolbarStyle={{
+          color: 'black',
+        }}
+        onEditorStateChange={onEditorStateChange}
+        toolbar={TOOLBAR_OPTIONS}
+        {...editorProps}
+      />
+    </div>
+  )
+}
