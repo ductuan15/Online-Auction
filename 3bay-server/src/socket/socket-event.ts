48b79641b8789508d8a855f237a8dc2aeba4f7ae
qhuydo <46428103+qhuydo@users.noreply.github.com>
2022-01-05 03:56:38 +0700

    [BE] Add generic socket emitter

---
+++ 3bay-server/src/socket/socket-event.ts
@@ -0,0 +1,3 @@
+export enum SocketEvent {
+  UPDATE_AUCTION = 'update_auction'
+}
--- 3bay-server/src/socket/socket.io.ts
+++ 3bay-server/src/socket/socket.io.ts
@@ -4,33 +4,40 @@ import { ExtendedError } from 'socket.io/dist/namespace.js'
 import passport from '../auth/passport.js'
 import Prisma from '@prisma/client'
 import c from 'ansi-colors'
+import { SocketEvent } from './socket-event.js'
 
 let io: Server
-declare module 'http' {
-  interface IncomingMessage {
-    user: Prisma.User
-  }
-}
+
 // convert a connect middleware to a Socket.IO middleware
 const wrap =
   (middleware: any) => (socket: Socket, next: (err?: ExtendedError) => void) =>
     middleware(socket.request, {}, next)
 
+declare module 'http' {
+  interface IncomingMessage {
+    user: Prisma.User
+  }
+}
+
+export function getSocket() {
+  return io ?? null
+}
+
 const onConnect = (socket: Socket) => {
-  console.log(c.bgMagenta(
-    `new connection ${socket.id}`
-  ))
+  console.log(c.bgMagenta(`new connection ${socket.id}`))
   // console.log(socket.request.user)
 
   socket.on('whoami', () => {
     // cb(socket.request.user ? socket.request.user.uuid : '')
-    console.log(`${socket.request.user.uuid} - ${socket.request.user.name} greeted you <3`)
+    console.log(
+      `${socket.request.user.uuid} - ${socket.request.user.name} greeted you <3`,
+    )
   })
 
   socket.on('disconnect', () => {
-    console.log(c.magenta(
-      `${socket.id} - ${socket.request.user.name} disconnected`
-    ))
+    console.log(
+      c.magenta(`${socket.id} - ${socket.request.user.name} disconnected`),
+    )
   })
 }
 
@@ -46,6 +53,18 @@ const initSocketIo = (server: HTTPServer): Server => {
   io.on('connection', onConnect)
 
   return io
+}
+
+export function emitEvent(
+  event: SocketEvent,
+  data: unknown,
+  cb?: (err: unknown, data: unknown) => void,
+) {
+  const io = getSocket()
+  if (!io) {
+    return null
+  }
+  return io.emit(event, data, cb)
 }
 
 export default initSocketIo
